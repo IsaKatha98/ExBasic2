57c5df5132cb1e6098e7a0b68766e283e54c542c
ikloerzer <ikloerzer@106-14.info2>
2022-09-22 13:28:01 +0200

    C2

--- ExBasic2/src/exbasic2/ExBasic2.java
+++ ExBasic2/src/exbasic2/ExBasic2.java
@@ -6,10 +6,23 @@ public class ExBasic2 {
 
 	public static void main(String[] args) {
 		
+		
+		//Le pedimos la edad al usuario
+		System.out.print("Introduzca su edad y pulse Intro: ");
+		
+		//Declaro la variable num donde se guarda el número introducido
+		int num;
+		
+		//Creo el escáner
 		Scanner sc= new Scanner (System.in);
-		System.out.print("Introduzca su edad:");
-		int numero= sc.nextInt()+1;
-		System.out.println(numero);
+		
+		//Guardo el número introducido y le añado uno
+		num= sc.nextInt()+1;
+		
+		//Mostramos el resultado
+		System.out.println("Esta será su edad el año que viene: "+num+" años.");
+		
+		//Cerramos el escáner
 		sc.close();
 		
 
